d64b8abb5ef04e3d5a904fb7d48d127764f5b14e
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-06-12 14:31:32 +0200

    feat: add SwiftLint as XCode buildtime plugin

--- Project.swift
+++ Project.swift
@@ -1,34 +1,42 @@
 import ProjectDescription
 
 let project = Project(
-    name: "Cardtier",
-    targets: [
-        .target(
-            name: "Cardtier",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.tuist.Cardtier",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["Cardtier/Sources/**"],
-            resources: ["Cardtier/Resources/**"],
-            dependencies: []
-        ),
-        .target(
-            name: "CardtierTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.CardtierTests",
-            infoPlist: .default,
-            sources: ["Cardtier/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "Cardtier")]
-        ),
-    ]
+  name: "Cardtier",
+  packages: [
+    .remote(
+      url: "https://github.com/SimplyDanny/SwiftLintPlugins",
+      requirement: .upToNextMajor(from: "0.59.1")
+    )
+  ],
+  targets: [
+    .target(
+      name: "Cardtier",
+      destinations: .iOS,
+      product: .app,
+      bundleId: "io.tuist.Cardtier",
+      infoPlist: .extendingDefault(
+        with: [
+          "UILaunchScreen": [
+            "UIColorName": "",
+            "UIImageName": "",
+          ]
+        ]
+      ),
+      sources: ["Cardtier/Sources/**"],
+      resources: ["Cardtier/Resources/**"],
+      dependencies: [
+        .package(product: "SwiftLintBuildToolPlugin", type: .plugin)
+      ]
+    ),
+    .target(
+      name: "CardtierTests",
+      destinations: .iOS,
+      product: .unitTests,
+      bundleId: "io.tuist.CardtierTests",
+      infoPlist: .default,
+      sources: ["Cardtier/Tests/**"],
+      resources: [],
+      dependencies: [.target(name: "Cardtier")]
+    ),
+  ]
 )
