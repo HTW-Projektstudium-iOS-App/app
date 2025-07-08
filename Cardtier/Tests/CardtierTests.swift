546e1bc55611d3006490bdd6b07f1e883ce9d183
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-07-08 23:33:23 +0200

    chore: formatting

--- Cardtier/Tests/CardtierTests.swift
+++ Cardtier/Tests/CardtierTests.swift
@@ -2,7 +2,8 @@ import Foundation
 import XCTest
 
 final class CardtierTests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
-    }
+  func test_twoPlusTwo_isFour() {
+    XCTAssertEqual(2 + 2, 4)
+  }
 }
+
